d61d21afdd2ecb3c06fa69e03b20aac0c329a405
sonyeseul <sonyeseuljam@naver.com>
2024-06-20 17:28:43 +0900

    [ADD] AdminPage.html 추가

--- src/main/java/com/example/letsGo/controller/pool/SearchPoolController.java
+++ src/main/java/com/example/letsGo/controller/pool/SearchPoolController.java
@@ -13,17 +13,6 @@ import java.util.Map;
 @Controller
 public class SearchPoolController {
 
-    @PostMapping("/swimpool/makeQuery")
-    @ResponseBody
-    public Map<String, String> searchPool(@RequestParam("area1") String area1, HttpServletRequest request) {
-        // 검색어를 세션에 저장
-        HttpSession session = request.getSession();
-        session.setAttribute("searchQuery", area1);
 
-        // JSON 형태로 응답
-        Map<String, String> response = new HashMap<>();
-        response.put("searchQuery", area1);
-        return response;
-    }
 
 }
